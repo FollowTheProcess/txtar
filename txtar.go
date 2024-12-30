f6db1c3a0755e4b64cf942b552d43b56a2c9e707
Tom Fleet <tom.fleet@chetwood.co>
2024-12-30 17:38:43 +0000

    Initial Archive functionality (#4)
    ## Summary
    
    <!-- Describe your changes in detail here, if it closes an open issue,
    include "Closes #<issue>" -->

---
+++ txtar.go
@@ -0,0 +1,130 @@
+// Copyright 2018 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package txtar re-implements and extends the original [golang.org/x/tools/txtar] package, making a number
+// of modifications and (hopefully) improvements to the package.
+//
+// No modifications are made to the txtar syntax, all txtar archives produced with this package are
+// compatible with the original.
+//
+// Improvements include:
+//
+//   - A CLI (cmd/txtar) that can "unzip" and "zip" a txtar archive to the real filesystem (and vice versa)
+//   - Files are stored as map[name]File so they may be looked up and operated on individually
+//   - Methods and functions are provided to help easily facilitate individual file editing
+//   - A number of useful interfaces are implemented to make an [Archive] more useful/flexible in the Go ecosystem
+//   - An ergonomic API for constructing an [Archive], rather than simply exposing struct fields
+//
+// # Original Package Documentation
+//
+// Package txtar implements a trivial text-based file archive format.
+//
+// The goals for the format are:
+//
+//   - be trivial enough to create and edit by hand.
+//   - be able to store trees of text files describing go command test cases.
+//   - diff nicely in git history and code reviews.
+//
+// Non-goals include being a completely general archive format,
+// storing binary data, storing file modes, storing special files like
+// symbolic links, and so on.
+//
+// # Txtar format
+//
+// A txtar archive is zero or more comment lines and then a sequence of file entries.
+// Each file entry begins with a file marker line of the form "-- FILENAME --"
+// and is followed by zero or more file content lines making up the file data.
+// The comment or file content ends at the next file marker line.
+// The file marker line must begin with the three-byte sequence "-- "
+// and end with the three-byte sequence " --", but the enclosed
+// file name can be surrounding by additional white space,
+// all of which is stripped.
+//
+// If the txtar file is missing a trailing newline on the final line,
+// parsers should consider a final newline to be present anyway.
+//
+// There are no possible syntax errors in a txtar archive.
+//
+// [golang.org/x/tools/txtar]: https://pkg.go.dev/golang.org/x/tools/txtar
+package txtar
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Archive is a collection of files.
+type Archive struct {
+	files   map[string]File // The files contained in the archive, stored by name
+	comment string          // The top level archive comment section
+}
+
+// Comment returns the top level archive comment.
+func (a Archive) Comment() string {
+	return a.comment
+}
+
+// Has returns whether the archive contains a file with the given name.
+func (a Archive) Has(name string) bool {
+	_, exists := a.files[name]
+	return exists
+}
+
+// Add adds a new [File] to the archive.
+//
+// File names must be unique within an archive so attempting to add a
+// duplicate file will result in an error.
+func (a *Archive) Add(name string, contents []byte) error {
+	if _, exists := a.files[name]; exists {
+		return fmt.Errorf("file with name %s already exists in archive", name)
+	}
+
+	a.files[name] = File{
+		name:     name,
+		contents: contents,
+	}
+	return nil
+}
+
+// Read returns the contents of the given file from the archive.
+//
+// If the file is not in the archive, an error will be returned.
+func (a Archive) Read(name string) ([]byte, error) {
+	file, exists := a.files[name]
+	if !exists {
+		return nil, fmt.Errorf("file %s not contained in the archive", name)
+	}
+	return file.contents, nil
+}
+
+// New returns a new [Archive], applying any number of initialisation options.
+func New(options ...Option) (*Archive, error) {
+	archive := &Archive{
+		files: make(map[string]File),
+	}
+
+	// Bubble up all the errors at once rather than forcing callers
+	// to play whack-a-mole
+	var errs error
+	for _, option := range options {
+		errs = errors.Join(errs, option(archive))
+	}
+
+	if errs != nil {
+		return nil, errs
+	}
+
+	return archive, nil
+}
+
+// File is a single file within an [Archive].
+type File struct {
+	name     string // The name of the file, so it can be accessed in methods
+	contents []byte // The contents of the file
+}
+
+// Name returns the name of the file.
+func (f File) Name() string {
+	return f.name
+}
